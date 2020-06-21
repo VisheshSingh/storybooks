47556eea8f72106e76fd1dc632d89aa465fb1b50
Vishesh Singh <VisheshSingh16@gmail.com>
2020-06-20 21:12:16 -0500

    Method override for DELETE ❎😵

--- routes/stories.js
+++ routes/stories.js
@@ -90,4 +90,16 @@ router.put('/:id', ensureAuth, async (req, res) => {
   }
 });
 
+// @desc    Delete a story
+// @method  DELETE /stories/:id
+router.delete('/:id', ensureAuth, async (req, res) => {
+  try {
+    await Story.remove({ _id: req.params.id });
+    res.redirect('/dashboard');
+  } catch (error) {
+    console.log(error);
+    res.render('error/500');
+  }
+});
+
 module.exports = router;
