cd2f9fb870e71e514bf5b09b8a12e23e9d4b2855
Vishesh Singh <VisheshSingh16@gmail.com>
2020-06-20 19:47:59 -0500

    Method override for PUT 🤶

--- routes/stories.js
+++ routes/stories.js
@@ -43,26 +43,51 @@ router.get('/', ensureAuth, async (req, res) => {
 // @desc    Show edit page
 // @method  GET /stories/edit/:id
 router.get('/edit/:id', ensureAuth, async (req, res) => {
-  //   try {
-  const story = await Story.findOne({
-    _id: req.params.id,
-  }).lean();
+  try {
+    const story = await Story.findOne({
+      _id: req.params.id,
+    }).lean();
 
-  if (!story) {
-    return res.render('error/404');
-  }
+    if (!story) {
+      return res.render('error/404');
+    }
 
-  if (story.user != req.user.id) {
-    res.redirect('/stories');
-  } else {
-    res.render('stories/edit', {
-      story,
-    });
+    if (story.user != req.user.id) {
+      res.redirect('/stories');
+    } else {
+      res.render('stories/edit', {
+        story,
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    res.render('error/500');
   }
-  //   } catch (error) {
-  //     console.log(error);
-  //     res.render('error/500');
-  //   }
+});
+
+// @desc    Update the story
+// @method  PUT /stories/:id
+router.put('/:id', ensureAuth, async (req, res) => {
+  try {
+    let story = await Story.findById(req.params.id).lean();
+
+    if (!story) {
+      return res.render('error/404');
+    }
+
+    if (story.user != req.user.id) {
+      res.redirect('/stories');
+    } else {
+      story = await Story.findOneAndUpdate({ _id: req.params.id }, req.body, {
+        new: true,
+        runValidators: true,
+      });
+      res.redirect('/dashboard');
+    }
+  } catch (error) {
+    console.log(error);
+    res.render('error/500');
+  }
 });
 
 module.exports = router;
